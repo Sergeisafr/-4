39fad6ee791171879b88751774567ea4a16559d4
Sergei <Sergeisafr@mail.ru>
2022-09-01 20:57:26 +0500

    Решение задачи 27 суммирует все значения  введеного числа

---
+++ Задача_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum1 = 0;
+int i = 0;
+{
+if (num > 9)
+
+    while (num > 0)
+    {
+        i = num % 10;
+        
+        num = num / 10;
+     if (i > 0)
+     sum1= sum1 + i;
+      
+    }
+
+ Console.WriteLine(sum1);  
+}

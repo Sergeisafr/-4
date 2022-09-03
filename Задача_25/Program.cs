36b53bebdc42266b3efbb94c9ab754f1a78833b9
Sergei <Sergeisafr@mail.ru>
2022-09-03 16:31:32 +0500

    Задачу 25 перевел в функцию

--- Задача_25/Program.cs
+++ Задача_25/Program.cs
@@ -2,14 +2,18 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int Degree (int num1, int num2)
+{
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 int vol = 1;
 for (int ind = 1; ind <= num2; ind++)
 
 vol = vol * num1;
 
-Console.WriteLine(vol);
-
+return vol;
+}
+int res = Degree (2, 4);
+Console.WriteLine(res);
